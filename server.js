de678ad3cd430114b51eb5f162fc41c06963ab1b
Shu Song <shoebox@gmail.com>
2017-09-23 18:30:48 -0400

    only run one copy

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const id = Number(process.env.NODE_APP_INSTANCE || 0);
 
 const hexo = require('hexo-cli/lib/hexo');
 
-hexo(__dirname, { _: ['server'], 's': true, 'p': 8000 + id, 'i': 'katelovenewyork.com' });
+hexo(__dirname, { _: ['server'], 's': true, 'p': 8000 + id });
